6e98c9f1c3eee324099b970b71b1aa0629e35931
kareemacosta <97877649+kareemacosta@users.noreply.github.com>
2023-01-10 04:29:20 -0500

    Create EtherWallet.sol

---
+++ EtherWallet.sol
@@ -0,0 +1,24 @@
+// SPDX-License-Identifier: MIT
+
+pragma solidity >=0.7.0 <0.9.0;
+
+contract EtherWallet {
+    address payable public owner;
+    
+    constructor() {
+        owner = payable(msg.sender);
+    }
+
+    receive() external payable {
+            
+    }
+
+    function withdraw(uint _amount) external {
+        require(msg.sender == owner, "Only the owner can call this method.");
+        payable(msg.sender).transfer(_amount);
+    }
+
+    function getBalance() external view returns (uint) {
+        return address(this).balance;
+    }
+}
